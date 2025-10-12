5d9cba092d8c03c7edcebf48aa87d553d2af51c0
Sarah Cobirzan <cobirzan.sarah@icloud.com>
2025-10-12 12:44:34 +0300

    Add project files.

---
+++ Sarah_Cobirzan_lab2/Data/Sarah_Cobirzan_lab2Context.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Sarah_Cobirzan_lab2.Models;
+
+namespace Sarah_Cobirzan_lab2.Data
+{
+    public class Sarah_Cobirzan_lab2Context : DbContext
+    {
+        public Sarah_Cobirzan_lab2Context (DbContextOptions<Sarah_Cobirzan_lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Sarah_Cobirzan_lab2.Models.Book> Book { get; set; } = default!;
+        public DbSet<Sarah_Cobirzan_lab2.Models.Publisher> Publisher { get; set; } = default!;
+    }
+}
---
+++ Sarah_Cobirzan_lab2/Pages/Books/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Sarah_Cobirzan_lab2.Data;
+using Sarah_Cobirzan_lab2.Models;
+
+namespace Sarah_Cobirzan_lab2.Pages.Books
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Sarah_Cobirzan_lab2.Data.Sarah_Cobirzan_lab2Context _context;
+
+        public IndexModel(Sarah_Cobirzan_lab2.Data.Sarah_Cobirzan_lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> Book { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Book = await _context.Book
+                .Include(b => b.Publisher)
+                .ToListAsync();
+        }
+    }
+}
